e636093f64de398a0b1a0ec835ea0628e9893aa5
Prasanth P <155311223+prasanth45bit@users.noreply.github.com>
2025-10-27 10:59:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization','token']
 }));
 
 app.use(express.json());
