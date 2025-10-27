16e201798f474ec9645c5524b4e6f97d3cc46091
Prasanth P <155311223+prasanth45bit@users.noreply.github.com>
2025-10-27 11:51:42 +0530

    Update CORS allowedHeaders to remove 'token'
    Removed 'token' from allowedHeaders in CORS configuration.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization','token']
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
